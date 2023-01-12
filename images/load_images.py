ebe89fb52381435e3e6aa7eb39970821e0044889
keinerex <Yurkov22Vl@gmail.com>
2023-01-12 21:08:08 +0700

    надписи и бегущий блёб

--- images/load_images.py
+++ images/load_images.py
@@ -2,23 +2,47 @@ from os import listdir
 
 from pygame import image, transform
 
+import constants
 from config import config
 
 
-def get_images(path):
-    return [image.load(path + "/" + file) for file in listdir(path)]
+def load_image(path):
+    sprite = image.load(path)
+    return sprite.subsurface((24, 30, 23, 18))
+
+
+def get_images(path, size=None, crop=False):
+    func = load_image if crop else image.load
+    if not size:
+        return [func(path + "/" + file) for file in listdir(path)]
+    return [transform.scale(func(path + "/" + file), size) for file in listdir(path)]
 
 
 class SlimeImages:
     def __init__(self, path):
-        self.walk = get_images(path + "/walk")
+        self.walk = get_images(path + "/walk", size=config.slime_size, crop=True)
 
 
-class Images(object):
-    background = transform.scale(image.load("sprites/field/field.png"), (config.screen_width, config.screen_height))
-    blue_slime = SlimeImages("sprites/slimes/blue")
-    green_slime = SlimeImages("sprites/slimes/green")
-    grey_slime = SlimeImages("sprites/slimes/grey")
-    purple_slime = SlimeImages("sprites/slimes/purple")
-    red_slime = SlimeImages("sprites/slimes/red")
-    coin = get_images("sprites/coin")
+class Images:
+    def __init__(self):
+        self.background = transform.scale(image.load("sprites/field/field.png"),
+                                          (config.screen_width, config.screen_height))
+        self.blue_slime = SlimeImages("sprites/slimes/blue")
+        self.green_slime = SlimeImages("sprites/slimes/green")
+        self.grey_slime = SlimeImages("sprites/slimes/grey")
+        self.purple_slime = SlimeImages("sprites/slimes/purple")
+        self.red_slime = SlimeImages("sprites/slimes/red")
+        self.coin = get_images("sprites/coin")
+
+    def __getitem__(self, item):
+        match item:
+            case constants.color.blue:
+                return self.blue_slime
+            case constants.color.green:
+                return self.green_slime
+            case constants.color.grey:
+                return self.grey_slime
+            case constants.color.purple:
+                return self.purple_slime
+            case constants.color.red:
+                return self.red_slime
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 import pygame
 
+import constants
 from config import config
 
 
@@ -9,5 +10,18 @@ def draw_available(display):
     for i in range(config.rows_count):
         for j in range(config.columns_count):
             if config.map[i][j] == 'o':
-                pygame.draw.rect(surface, (0, 255, 0, 255), (j * config.cell_size[0], i * config.cell_size[1], *config.cell_size), 0, 20)
+                pygame.draw.rect(surface, (0, 255, 0, 255),
+                                 (j * config.cell_size[0], i * config.cell_size[1], *config.cell_size), 0, 20)
     display.blit(surface, (0, 0))
+
+
+def draw_information(display, money, money_pos, score, score_pos):
+    score = config.font.render(f"Score: {score}", False, (0, 0, 0))
+    money = config.font.render(f"Balance: {money}", False, (0, 0, 0))
+    money_width = money.get_size()[0]
+    score_width = score.get_size()[0]
+    display.blit(money, money_pos)
+    display.blit(config.font.render("Ⓞ", False, (255, 255, 0)), (money_pos[0] + money_width, money_pos[1]))
+    display.blit(score, score_pos)
+    display.blit(config.font.render("★", False, (255, 255, 0)), (score_pos[0] + score_width, score_pos[1]))
+
